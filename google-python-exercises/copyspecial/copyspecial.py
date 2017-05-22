9f3e64fb6dd4d57f6e838a54cb1da2c1ed2c5d93
Vikas Jain <vikasjain081291@live.com>
2017-05-22 14:10:38 +0530

    copyspecial exercise completed

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -10,16 +10,33 @@ import sys
 import re
 import os
 import shutil
-import commands
+import subprocess
 
 """Copy Special exercise
 """
 
 # +++your code here+++
 # Write functions and modify main() to call them
+def find_special(dir):
+  filenames= os.listdir(dir)
+  path=[]
+  for filename in filenames:
+    if re.search(r'__\w+__', filename):
+      path.append(os.path.abspath(filename))
+  return path
 
+def copy(paths, todir):
+  for path in paths:
+    shutil.copy(path,todir)
+  return
 
-
+def zipit(path, tozip):
+  cmd = 'zip -j' + tozip+'' +''.join(path)
+  (status,output) = subprocess.getstatusoutput(cmd)
+  print(status)
+  print(output)
+  return
+  
 def main():
   # This basic command line argument parsing code is provided.
   # Add code to call your functions below.
@@ -28,7 +45,7 @@ def main():
   # which is the script itself.
   args = sys.argv[1:]
   if not args:
-    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+    print ("usage: [--todir dir][--tozip zipfile] dir [dir ...]");
     sys.exit(1)
 
   # todir and tozip are either set from command line
@@ -45,11 +62,19 @@ def main():
     del args[0:2]
 
   if len(args) == 0:
-    print "error: must specify one or more dirs"
+    print ("error: must specify one or more dirs")
     sys.exit(1)
 
   # +++your code here+++
-  # Call your functions
+  # Call your functions 
+  for dir in args:
+    path = find_special(dir)
+    print(todir)
+    if todir:
+      copy(path, todir)
+    if tozip:
+      print(tozip)
+      zipit(path, tozip)
   
 if __name__ == "__main__":
   main()
